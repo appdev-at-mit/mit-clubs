e8328dea08eb9df7008bcd0bda53ba00962fc0a3
Hailey Pan <haileyrpan@gmail.com>
2025-04-29 14:28:11 -0400

    center search icon

--- client/src/components/pages/Clubs.jsx
+++ client/src/components/pages/Clubs.jsx
@@ -404,7 +404,7 @@ const Clubs = () => {
               onChange={(e) => setSearchTerm(e.target.value)}
               className="w-full px-4 py-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
             />
-            <FaSearch className="absolute top-2.5 right-4 text-gray-400" />
+            <FaSearch className="absolute top-1/2 right-4 -translate-y-1/2 text-gray-400" />
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {filteredClubs
