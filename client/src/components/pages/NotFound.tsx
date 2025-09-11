ff66c9d462d475d3cd7a65da936c9b2ca5e5d605
Hailey Pan <haileyrpan@gmail.com>
2025-09-11 08:49:33 -0400

    notfound page

--- client/src/components/pages/NotFound.tsx
+++ client/src/components/pages/NotFound.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import Logo from "../../assets/logo-with-words.png";
 
-function NotFound() {
+const NotFound = () => {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <p>The page you requested couldn't be found.</p>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 mx-3">
+      <img src={Logo} alt="MIT Clubs Logo" className="w-64 mb-4" />
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        something went wrong
+      </h1>
+      <p className="text-lg text-gray-600 mb-8">
+        The page you are looking for does not exist.
+      </p>
     </div>
   );
 };
